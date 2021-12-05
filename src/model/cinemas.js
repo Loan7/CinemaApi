7ac3f855be6800ee70bf83de71d5400b28345254
Loan7 <olivera_rox@hotmail.com>
2021-12-05 00:47:12 -0300

    correção sintaxe

--- src/model/cinemas.js
+++ src/model/cinemas.js
@@ -2,7 +2,7 @@ class Cinemas{
 
     constructor(nome, endereco, numero, bairro, cidade, estado, uf){
         this.nome = this.verificaNome(nome)
-        this.endereco = this.verficaEndereco(endereco)
+        this.endereco = this.verificaEndereco(endereco)
         this.numero = this.verificaNumero(numero)
         this.bairro = this.verificaBairro(bairro)
         this.cidade = this.verificaCidade(cidade)
